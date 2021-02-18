b4c021cd76106b3539fa9b2d988016402e210869
juan-skill <1294@holbertonschool.com>
2021-02-18 17:47:08 +0000

    [add] Upload file 	new file:   0-minoperations.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+The 0-minOperations module supplies one function, minOperations().
+"""
+
+
+def minOperations(n):
+    """ calculates the fewest number of operations needed
+        to result in exactly n H charactes in the file """
+    pass
