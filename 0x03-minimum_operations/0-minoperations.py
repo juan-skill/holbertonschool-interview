0935886421b1cdc310e67844acfa9ab01e181f8f
juan-skill <1294@holbertonschool.com>
2021-02-19 03:23:31 +0000

    [add] Minimum Operations: write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file. 	modified:   0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,4 +7,24 @@ The 0-minOperations module supplies one function, minOperations().
 def minOperations(n):
     """ calculates the fewest number of operations needed
         to result in exactly n H charactes in the file """
-    pass
+
+    divisor = 2
+    number_operations = 0
+
+    if n <= 1:
+        return 0
+
+    while True:
+
+        if (n % divisor) == 0:
+
+            n = n // divisor
+            number_operations += divisor
+
+        else:
+            divisor += 1
+
+        if n <= 1:
+            break
+
+    return number_operations
